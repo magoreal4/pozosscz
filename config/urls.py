b51f9a11d5bb6bc444fc6d50bed3ab844f891d3a
magoreal4 <magoreal4@gmail.com>
2022-04-25 23:46:32 -0400

    first commit

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticSitemap
 
+from django.views.generic.base import TemplateView
+
 sitemaps = {
     'static': StaticSitemap,
 }
@@ -19,7 +21,8 @@ urlpatterns = [
     path('', include('main.urls')),
     path('mapa/', include('mapa.urls')),
     path('contact/', include('fcontact.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"))
 ]
 
 if settings.DEBUG:
